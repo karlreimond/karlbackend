f0d91b6925af5c59450447fcdc9a3657948fba20
karlreimond <karlreim@tlu.ee>
2021-09-06 10:01:24 +0200

    frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Item from './components/Item';
+function App() {
+  return (
+    <div>
+      Tere
+      <Item />
+      <Item />
+    </div>
+  );
+}
+
+export default App;
